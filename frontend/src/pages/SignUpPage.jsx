613e67a8d4d1e61618b0e1bb62e6725941cf60ff
Aryan Kumar Swain <aryannn.ks@gmail.com>
2026-01-01 16:29:19 +0530

    updated

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -70,7 +70,7 @@ const SignUpPage = () => {
                 <input
                   type="text"
                   className="input input-bordered w-full pl-10"
-                  placeholder="John Doe"
+                  placeholder="Aryan Kumar Swain"
                   value={formData.fullName}
                   onChange={(e) =>
                     setFormData({ ...formData, fullName: e.target.value })
